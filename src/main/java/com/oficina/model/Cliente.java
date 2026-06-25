5d34855f62c320402fe991728a2e1a718fbfabf9
wunderlich-15 <guilhermewunderlich@hotmail.com>
2026-06-24 22:04:07 -0300

    removido classe pessoa

--- src/main/java/com/oficina/model/Cliente.java
+++ src/main/java/com/oficina/model/Cliente.java
@@ -7,7 +7,9 @@ import lombok.NoArgsConstructor;
 @Data 
 @AllArgsConstructor 
 @NoArgsConstructor
-public class Cliente extends Pessoa{
+public class Cliente {
+    private Long id;
+    private String nome;
     private String cpf;
     private String telefone;
 }
